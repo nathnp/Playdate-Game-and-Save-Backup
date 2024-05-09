3705fcd668b5f0c4e2bfc55a243eb620d7f4e69a
Nathan <57642318+nathnp@users.noreply.github.com>
2024-05-09 13:40:56 -0400

    Added LInux scripts

---
+++ src/LinuxM/PDBackup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+folder="/media/$USER/PLAYDATE" # <- Put path to Playdate's root here
+
+if [ ! -d "$folder" ]; then
+	echo "Please connect your Playdate in data disk mode"
+	echo "You can do this by going to Settings -> System -> Reboot to Data Disk -> OK on the Playdate"
+	exit 1
+fi
+
+printf "Enter date, no spaces please "
+read tdate
+
+printf "Copying saves, this may take a moment\n"
+cp -r $folder/Data .
+
+printf "Copying games, this WILL take a while\n"
+cp -r $folder/Games .
+
+printf "Tarballing Saves and Games\n"
+tar cf PDSavesNGames$tdate.tar Data/ Games/
+
+printf "Gziping tarball\n"
+gzip PDSavesNGames$tdate.tar
+
+rm -rf Data
+rm -rf Games
+
+printf "Done\n"
